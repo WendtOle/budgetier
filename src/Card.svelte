5d6f6aaae14cea0eea4adeca3bc4ede63446980c
Olee <olee@Oles-Air.fritz.box>
2024-09-10 22:09:30 +0200

    Add tailwind and put everything on cards

---
+++ src/Card.svelte
@@ -0,0 +1,16 @@
+<div class="card">
+	<slot />
+</div>
+
+<style>
+	.card {
+		background-color: white;
+		border-radius: 5px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		padding: 1rem;
+		margin: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Card from '../Card.svelte';
 	import {
 		total,
 		fixCosts,
@@ -16,57 +17,72 @@
 	<meta name="description" content="Simple budgetier app - Budgetier" />
 </svelte:head>
 
-<h1>Budgetier</h1>
-<div class="col">
-	<div class="row">
-		<label for="total">Total:</label>
-		<input id="total" type="number" bind:value={$total} />
-	</div>
-	<div class="row">
-		<label for="total">Fixkosten:</label>
-		<input
-			type="range"
-			min="0"
-			max={$maxFixedCosts}
-			step="1"
-			bind:value={$fixCosts}
-			disabled={$maxFixedCosts === 0}
-		/>
+<div>
+	<Card>
+		<div>
+			<label for="total">Total:</label>
+			<input
+				class="bg-white border border-gray-300 rounded-lg p-2"
+				id="total"
+				type="number"
+				bind:value={$total}
+			/>
+		</div>
+	</Card>
+	<Card>
+		<div>
+			<label for="total">Fixkosten:</label>
+			<input
+				type="range"
+				min="0"
+				max={$maxFixedCosts}
+				step="1"
+				bind:value={$fixCosts}
+				disabled={$maxFixedCosts === 0}
+			/>
+			<span>{$fixCosts} €</span>
+		</div>
 		{#if $total !== 0}
-			<span>{$fixCosts} € - {(($fixCosts / $total) * 100).toPrecision(3)} % of total budget</span>
+			<span>{(($fixCosts / $total) * 100).toPrecision(3)} % of total budget</span>
 		{/if}
-	</div>
-	<div class="row">
-		<label for="total">Variable costs:</label>
-		<input
-			type="range"
-			min="0"
-			max={$maxVariableCosts}
-			step="1"
-			bind:value={$variableCosts}
-			disabled={$maxVariableCosts === 0}
-		/>
+	</Card>
+	<Card>
+		<div>
+			<label for="total">Variable costs:</label>
+			<input
+				type="range"
+				min="0"
+				max={$maxVariableCosts}
+				step="1"
+				bind:value={$variableCosts}
+				disabled={$maxVariableCosts === 0}
+			/>
+			<span>{$variableCosts} €</span>
+		</div>
 		{#if $total !== 0}
-			<span
-				>{$variableCosts} € - {(($variableCosts / $total) * 100).toPrecision(3)} % of total budget</span
-			>
+			<div>
+				{(($variableCosts / $total) * 100).toPrecision(3)} % of total budget
+			</div>
 		{/if}
-	</div>
-	<div class="row">
-		<label for="total">Savings:</label>
-		<input
-			type="range"
-			min="0"
-			max={$maxSavings}
-			step="1"
-			bind:value={$savings}
-			disabled={$maxSavings === 0}
-		/>
+	</Card>
+	<Card>
+		<div>
+			<label for="total">Savings:</label>
+			<input
+				type="range"
+				min="0"
+				max={$maxSavings}
+				step="1"
+				bind:value={$savings}
+				disabled={$maxSavings === 0}
+			/>
+			<span>{$savings} €</span>
+		</div>
 		{#if $total !== 0}
-			<span>{$savings} € - {(($savings / $total) * 100).toPrecision(3)} % of total budget</span>
+			<span>{(($savings / $total) * 100).toPrecision(3)} % of total budget</span>
 		{/if}
-	</div>
-	<div>
+	</Card>
+	<Card>
 		<span>Rest which is not assigned yet: {$rest} €</span>
-	</div>
+	</Card>
 </div>
